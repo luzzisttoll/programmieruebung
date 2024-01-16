9a4536648a99415c201cbeae627ff4e14bdcce2b
luzzisttoll <91245180+luzzisttoll@users.noreply.github.com>
2024-01-16 13:08:19 +0100

    comments

--- stringmirrored/test_string_mirrored/Test_String_Program.cs
+++ stringmirrored/test_string_mirrored/Test_String_Program.cs
@@ -7,43 +7,41 @@ namespace test_string_mirrored
     {
         [Test]
         public static void TestMirroredString_IdenticalStrings_ReturnsZero()
-        {
-            // Arrange
+        
             string str1 = "bla";
             string str2 = "alb";
 
-            // Act
             int result = StringMirror.Program.MirroredString(str1, str2);
 
-            // Assert
+
             Assert.AreEqual(0, result);
         }
 
         [Test]
         public void TestMirroredString_DifferentStrings_ReturnsPosition()
         {
-            // Arrange
+
             string str1 = "blebla";
             string str2 = "albalb";
 
-            // Act
+
             int result = StringMirror.Program.MirroredString(str1, str2);
 
-            // Assert
+            
             Assert.AreEqual(3, result);
         }
 
         [Test]
         public void TestMirroredString_UnequalLengthStrings_ReturnsZero()
         {
-            // Arrange
+            
             string str1 = "abc";
             string str2 = "defg";
 
-            // Act
+            
             int result = StringMirror.Program.MirroredString(str1, str2);
 
-            // Assert
+            
             Assert.AreEqual(0, result);
         }
     }
